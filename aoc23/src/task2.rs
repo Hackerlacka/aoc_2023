b7dd5f647215c7f2fe408ee1d0cc08804c5b8340
Hackerlacka <robin.robbe96@gmail.com>
2023-12-25 16:26:47 +0100

    Day 23 WIP

--- aoc23/src/task2.rs
+++ aoc23/src/task2.rs
@@ -8,15 +8,15 @@ mod tests {
     #[test]
     fn test_example_input() {
         let mut trail_map = TrailMap::parse("../input/23_1_example_input.txt");
-        let longest_hike = trail_map.find_longest_hike(false);
+        let longest_hike = trail_map.find_longest_hike(true);
 
-        assert_eq!(longest_hike, 94);
+        assert_eq!(longest_hike, 154);
     }
 }
 
 pub fn run_task() {
     let mut trail_map = TrailMap::parse("input/23_1_input.txt");
-    let longest_hike = trail_map.find_longest_hike(false);
+    let longest_hike = trail_map.find_longest_hike(true);
 
-    println!("Longest hike: {}", longest_hike);
+    println!("Longest hike (ignoring slopes): {}", longest_hike);
 }
--- aoc23/src/utils.rs
+++ aoc23/src/utils.rs
@@ -16,7 +16,6 @@ enum TileType {
 type Pos = (usize, usize);
 
 struct TestTile {
-    // TODO: Could add "enter_from" for optimzation (or just check previously visited tile)
     pos: Pos,
     cost: u64,
     visited_pos: Vec<Pos>,
@@ -24,7 +23,8 @@ struct TestTile {
 }
 
 struct Tile {
-    tile_type: TileType
+    tile_type: TileType,
+    //highest_cost: u64
 }
 
 pub struct TrailMap {
@@ -44,6 +44,20 @@ impl SlopeType {
 }
 
 impl TestTile {
+    fn from_other_single(other: TestTile, pos: Pos) -> TestTile {
+        TestTile { pos: pos, cost: other.cost, visited_pos: other.visited_pos, previously_visited_pos: other.previously_visited_pos }
+    }
+
+    fn from_other_multiple(other: TestTile, positions: Vec<Pos>) -> VecDeque<TestTile> {
+        let visited_pos_rc = Rc::new(other.visited_pos);
+
+        return positions.into_iter().map(|pos| {
+            let mut previously_visited_pos = other.previously_visited_pos.clone(); // TODO: Does this clone RCs?
+            previously_visited_pos.push(Rc::clone(&visited_pos_rc));
+            TestTile { pos: pos, cost: other.cost, visited_pos: Vec::new(), previously_visited_pos: previously_visited_pos }
+        }).collect();
+    }
+
     fn new(pos: Pos) -> TestTile {
         TestTile { pos: pos, cost: 0, visited_pos: Vec::new(), previously_visited_pos: Vec::new() }
     }
@@ -62,20 +76,125 @@ impl Tile {
 }
 
 impl TrailMap {
-    fn move_to_tile(test_tile: TestTile, map: &mut Vec<Vec<Tile>>, test_tile_queue: &mut VecDeque<TestTile>, test_tile_finished: &mut Vec<TestTile>, end_pos: &Pos) {
-        // TODO: Check if been at tile before
+    fn has_been_on_tile(pos: &Pos, test_tile: &TestTile) -> bool {
+        if test_tile.visited_pos.contains(pos) 
+            || test_tile.previously_visited_pos.iter().any(|visited| visited.contains(pos)) {
+            return true;
+        }
 
-        // TODO: Add self to visited tiles
+        // let len: usize = test_tile.visited_pos.len() + test_tile.previously_visited_pos.iter().map(|visited| visited.len()).sum::<usize>();
+        // println!("Has been on tile with len: {}", len);
 
-        // TODO: Increase cost
+        return false;
+    }
+
+    fn add_next_steps(test_tile: TestTile, map: &Vec<Vec<Tile>>, test_tile_queue: &mut VecDeque<TestTile>, ignore_slopes: bool) {
+        let height = map.len();
+        let width = map.first().unwrap().len();
+        let mut possible_next_pos = Vec::new();
+
+        let pos = &test_tile.pos;
+        if pos.0 > 0 {
+            possible_next_pos.push((pos.0 - 1, pos.1))
+        }
+        if pos.0 + 1 < height {
+            possible_next_pos.push((pos.0 + 1, pos.1))
+        }
+        if pos.1 > 0 {
+            possible_next_pos.push((pos.0, pos.1 - 1))
+        }
+        if pos.1 + 1 < width {
+            possible_next_pos.push((pos.0, pos.1 + 1))
+        }
+
+        for i in (0..possible_next_pos.len()).rev() {
+            let next_pos = &possible_next_pos[i];
+
+            // Remove visited tiles visited
+            if Self::has_been_on_tile(next_pos, &test_tile) {
+                possible_next_pos.remove(i);
+                continue;
+            }
+            
+            // Check tile type
+            let tile_type = &map[next_pos.0][next_pos.1].tile_type;
+            match tile_type {
+                TileType::Forest => {
+                    possible_next_pos.remove(i);
+                    continue;
+                },
+                TileType::Slope(slope_type) => {
+                    if ignore_slopes {
+                        continue;
+                    }
+
+                    // Check if it is possible to walk (depending on direction)
+                    let y_diff = next_pos.0 as isize - pos.0 as isize;
+                    let x_diff = next_pos.1 as isize - pos.1 as isize;
+                    match slope_type {
+                        SlopeType::North => {
+                            if y_diff > 0 {
+                                possible_next_pos.remove(i);
+                                continue;
+                            }
+                        },
+                        SlopeType::West => {
+                            if x_diff > 0 {
+                                possible_next_pos.remove(i);
+                                continue;
+                            }
+                        },
+                        SlopeType::South => {
+                            if y_diff < 0 {
+                                possible_next_pos.remove(i);
+                                continue;
+                            }
+                        },
+                        SlopeType::East => {
+                            if x_diff < 0 {
+                                possible_next_pos.remove(i);
+                                continue;
+                            }
+                        }
+                    }
+                },
+                _ => ()
+            }
+        }
+
+        // Add new test tiles to queue
+        if possible_next_pos.len() == 1 {
+            test_tile_queue.push_back(TestTile::from_other_single(test_tile, possible_next_pos.remove(0)));
+        } else if possible_next_pos.len() > 1 {
+            // TODO: I guess this appends to the back of the queue?
+            test_tile_queue.append(&mut TestTile::from_other_multiple(test_tile, possible_next_pos));
+        }
+    }
+
+    fn move_to_tile(mut test_tile: TestTile, map: &mut Vec<Vec<Tile>>, test_tile_queue: &mut VecDeque<TestTile>, test_tile_finished: &mut Vec<TestTile>, end_pos: &Pos, ignore_slopes: bool) {
+        // Add self to visited tiles
+        test_tile.visited_pos.push(test_tile.pos);
+
+        // Increase cost
+        test_tile.cost += 1;
         
-        // TODO: Check if at finish tile, if so add to test_tile_finished and return
+        // Check if at finish tile, if so add to test_tile_finished and return
+        if test_tile.pos == *end_pos {
+            test_tile_finished.push(test_tile);
+            return;
+        }
 
-        // TODO: Get tile
+        // TODO: Check if tile has been visited before with more expensive cost (not possible?)
+        // let pos = &test_tile.pos;
+        // let tile = &mut map[pos.0][pos.1];
+        // if tile.highest_cost > test_tile.cost {
+        //     return;
+        // }
+        // tile.highest_cost = test_tile.cost;
 
-        // TODO: Add tile to visited tiles
 
-        // TODO: Determine next steps + add them to test_tile_queue (Rc to visited paths)
+        // Determine and add next steps
+        Self::add_next_steps(test_tile, map, test_tile_queue, ignore_slopes);
     }
 
     fn determine_start_and_end_tile_pos(map: &Vec<Vec<Tile>>) -> (Pos, Pos) {
@@ -98,7 +217,7 @@ impl TrailMap {
         return (start_pos.unwrap(), end_pos.unwrap());
     }
 
-    pub fn find_longest_hike(&mut self) -> u64 {
+    pub fn find_longest_hike(&mut self, ignore_slopes: bool) -> u64 {
         // "if you step onto a slope tile, your next step must be downhill (in the direction the arrow is pointing)"
         // "never step onto the same tile twice"
 
@@ -122,11 +241,11 @@ impl TrailMap {
 
         // Test move to tiles
         while let Some(test_tile) = test_tile_queue.pop_front() {
-            Self::move_to_tile(test_tile, &mut self.map, &mut test_tile_queue, &mut test_tile_finished, &end_pos);
+            Self::move_to_tile(test_tile, &mut self.map, &mut test_tile_queue, &mut test_tile_finished, &end_pos, ignore_slopes);
         }
 
         let max_cost = test_tile_finished.iter().map(|test_tile| test_tile.cost).max().unwrap();
-        return max_cost;
+        return max_cost - 1; // -1 to remove start tile step
     }
 
     pub fn parse(file: &str) -> TrailMap {
