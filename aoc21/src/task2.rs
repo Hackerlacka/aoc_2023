ea750e6b0aae555f3108b3180010809a53930840
Hackerlacka <robin.robbe96@gmail.com>
2023-12-28 20:31:30 +0100

    Solved day 21

--- aoc21/src/task2.rs
+++ aoc21/src/task2.rs
@@ -1,58 +1,10 @@
 use crate::utils::FarmMap;
 
-#[cfg(test)]
-mod tests {
-
-    use super::*;
-
-    #[test]
-    fn test_example_input_1() {
-        let farm_map = FarmMap::parse("../input/21_1_example_input.txt");
-
-        let steps = vec![6, 10, 50, 100, 500, 1000]; // , 5000
-        let expected_results = vec![16, 50, 1594, 6536, 167004, 668697]; // , 16733044
-
-        aoc_helper::print_time_now();
-        for (step, expected_result) in steps.iter().zip(expected_results.iter()) {
-            let garden_plots_reached = farm_map.calculate_garden_plots_reached(*step, true);
-
-            assert_eq!(garden_plots_reached, *expected_result);
-
-            println!("Steps: {} OK", step);
-            aoc_helper::print_time_now();
-        }
-    }
-
-    #[test]
-    fn test_example_input_2() {
-        let farm_map = FarmMap::parse("../input/21_1_example_input.txt");
-        let steps: u64 = 1000;
-        
-        farm_map.output_garden_plots_reached_difference(steps);
-    }
-
-    // cargo test -p aoc21 --release test_example_input_3 -- --nocapture
-    #[test]
-    fn test_example_input_3() {
-        let farm_map = FarmMap::parse("../input/21_1_example_input.txt");
-        let steps: u64 = 5000;
-        
-        farm_map.calculate_garden_plots_reached_opt(steps);
-    }
-
-    #[test]
-    fn test_input() {
-        let farm_map = FarmMap::parse("../input/21_1_input.txt");
-        let steps: u64 = 1000;
-        
-        farm_map.output_garden_plots_reached_difference(steps);
-    }
-}
-
+// Test input and real input does not share the same properties
 pub fn run_task() {
     let farm_map = FarmMap::parse("input/21_1_input.txt");
     let steps = 26501365;
     let garden_plots_reached = farm_map.calculate_garden_plots_reached_opt(steps);
 
-    println!("Garden plots reached after {} steps: {}", steps, garden_plots_reached);
+    println!("Garden plots reached in {} steps: {}", steps, garden_plots_reached);
 }
--- aoc21/src/utils.rs
+++ aoc21/src/utils.rs
@@ -1,6 +1,4 @@
 use std::collections::{HashSet, VecDeque};
-use std::fs::OpenOptions;
-use std::io::Write;
 
 pub struct FarmMap {
     map: Vec<Vec<char>>
@@ -32,7 +30,6 @@ impl FarmMap {
         panic!("Could not find start position");
     }
 
-    // TODO: Move to aoc_helper?
     fn positive_modulo(a: isize, b: usize) -> usize {
         let mut res = a % (b as isize);
         if res < 0 {
@@ -124,234 +121,36 @@ impl FarmMap {
         return unique_plots_reached;
     }
 
-    // TODO: Remove?
-    fn get_all_garden_plots_reached_count(&self, steps: u64) -> Vec<u64> {
-        let unique_plots_reached = self.calculate_unique_plots_reached(steps, true);
-
-        let mut res: Vec<_> = Vec::new();
-        for step in 0..(steps + 1) {
-            res.push(Self::calculate_unique_pos(step, &unique_plots_reached));
-        }
-
-        return res;
-    }
-
-    pub fn output_garden_plots_reached_difference(&self, steps: u64) {
-        let mut file = OpenOptions::new()
-        .write(true)
-        .append(false)
-        .create(true)
-        .open("aoc21_part2.txt")
-        .unwrap();
-
-        let unique_plots_reached = self.calculate_unique_plots_reached(steps, true);
-
-        let counts: Vec<usize> = unique_plots_reached.iter().map(|set| set.len()).collect();
-
-        let width_height = self.map.len();
-
-        let mut diffs = Vec::new();
-        for (i, count) in counts.iter().enumerate() {
-            let diff: i64;
-            if i < width_height {
-                diff = *count as i64;
-            } else {
-                diff = *count as i64 - counts[i - width_height] as i64;
-            }
-            diffs.push(diff);
-
-            if let Err(e) = writeln!(file, "{}", diff) {
-                eprintln!("Couldn't write to file: {}", e);
-            }
-        }
-
-        // let garden_plots_reached_count = self.get_all_garden_plots_reached_count(steps);
-        // let width_height = self.map.len();
-        // let mut diffs = Vec::new();
-        // for (i, count) in garden_plots_reached_count.iter().enumerate() {
-        //     let diff: i64;
-        //     if i == 0 {
-        //         diff = *count as i64;
-        //     } else {
-        //         diff = *count as i64 - garden_plots_reached_count[i - 1] as i64;
-        //     }
-        //     diffs.push(diff);
-
-        //     if let Err(e) = writeln!(file, "{}", diff) {
-        //         eprintln!("Couldn't write to file: {}", e);
-        //     }
-        // }
-
-        // if let Err(e) = writeln!(file, "") {
-        //     eprintln!("Couldn't write to file: {}", e);
-        // }
-
-        // let mut diff_diffs = Vec::new();
-        // for (i, tmp_diff) in diffs.iter().enumerate() {
-        //     let diff: i64;
-        //     if i < width_height {
-        //         diff = *tmp_diff;
-        //     } else {
-        //         diff = tmp_diff - diffs[i - width_height];
-        //     }
-        //     diff_diffs.push(diff);
-
-        //     if let Err(e) = writeln!(file, "{}", diff) {
-        //         eprintln!("Couldn't write to file: {}", e);
-        //     }
-        // }
-
-        
-
-    }
-
-
-    fn calculate_a_b_numbers_n(counts: &Vec<usize>, diffs: &Vec<i64>, pattern_start: usize, n: usize, steps_modulus: u64) -> (i64, i64) {
-        let a_start_index = pattern_start - n;
-        let a_all_numbers = &counts[a_start_index..(a_start_index + n)];
-        let a_partial_sum: i64 = a_all_numbers.iter().enumerate().map(|(i, val)| {
-            let j = a_start_index + i; // TODO: If n is odd, one of these need to add +1
-            if j % 2 == steps_modulus as usize {
-                *val as i64
-            } else {
-                0
-            }
-        }).sum();
-
-        let b_all_numbers = &diffs[pattern_start..(pattern_start + n)];
-        let b_partial_sum: i64 = b_all_numbers.iter().enumerate().map(|(i, val)| {
-            let j = pattern_start + i;
-            if j % 2 == steps_modulus as usize {
-                *val as i64
-            } else {
-                0
-            }
-        }).sum();
-
-        return (a_partial_sum, b_partial_sum);
-    }
-
-    fn find_pattern_start_n(values: &Vec<i64>, n: usize) -> usize {
-        let mut pattern_start = 0;
-        for i in 0..values.len() {
-            if i + n >= values.len() {
-                panic!("Found no pattern!");
-            }
-
-            // TODO: Choosing n here would perhaps be expensive?
-            if (0..5).map(|j| values[i + j] == values[i + j + n]).all(|b| b) {
-                println!("Possible pattern found at i: {} + {}", i, n);
-                pattern_start = i;
-                break;
-            }
-        }
-
-        return pattern_start;
-    }
-
-    fn calculate_diff_n(values: &Vec<usize>, n: usize) -> Vec<i64> {
-        let mut diffs = Vec::new();
-
-        for (i, count) in values.iter().enumerate() {
-            let diff: i64;
-            if i < n {
-                diff = *count as i64;
-            } else {
-                diff = *count as i64 - values[i - n] as i64;
-            }
-            diffs.push(diff);
-        }
-
-        return diffs;
-    }
-
     pub fn calculate_garden_plots_reached_opt(&self, steps: u64) -> u64 {
-        let steps_calculated = 750; // TODO: Not sure how to make this adjust after "steps"
-        let unique_plots_reached = self.calculate_unique_plots_reached(steps_calculated, true);
+        let map_len = self.map.len() as u64;
+        let half_map_len = map_len / 2 as u64;
 
-        // Step 0 == ..., step 1 == ..., ..., step n = ...
-        let counts: Vec<usize> = unique_plots_reached.iter().map(|set| set.len()).collect(); 
-        
-        // Map width is same as height
-        let width_height = self.map.len();
-        
-        // Calculate diffs between every n elements (n = width_height)
-        let diffs = Self::calculate_diff_n(&counts, width_height);
+        // Reachable tiles (ignoring rocks) (2n + 1)^2 / 2
+        // +1 for center tile
 
-        // Find pattern in the diffs
-        let pattern_start = Self::find_pattern_start_n(&diffs, width_height);
+        // Farm plots reached, T(x) = ax^2 + bx + c
+        // Gather three sample points
+        let sample_points = vec![half_map_len, half_map_len + map_len, half_map_len + 2 * map_len];
+        let plots_reached_sp: Vec<u64> = sample_points.iter().map(|sp| {
+            self.calculate_garden_plots_reached(*sp, true)
+        }).collect();
 
-        // To know garden plots reached for "steps" (large) we need all unique_plots_reached on the way, that are on the same modulus (even or odd)
-        let steps_modulus = steps % 2;
+        // Derive constants a, b and c
+        // P(0) = a * 0^2 + b * 0 + c == c
+        let c = plots_reached_sp[0];
 
-        let (a_partial_sum, b_partial_sum) = Self::calculate_a_b_numbers_n(&counts, &diffs, pattern_start, width_height, steps_modulus);
-        
-        let steps_remaining = steps - pattern_start as u64 + 1; // +1 to include the step we can aiming for
-        let repetitions = (steps_remaining / width_height as u64) as i64;
+        // b = (4P(1) - 3P(0) - P(2)) / 2
+        let b = (4 * plots_reached_sp[1] - 3 * plots_reached_sp[0] - plots_reached_sp[2]) / 2;
 
-        // Using "natural numbers" formula i.e. sum k=1..n for k = n(n + 1) / 2
-        let sum_repeat = repetitions * a_partial_sum + repetitions * (repetitions + 1) / 2 * b_partial_sum;
-        println!("Sum repeat: {}", sum_repeat);
+        // a = P(1) - P(0) - b
+        let a = plots_reached_sp[1] - plots_reached_sp[0] - b;
 
-        // 18319050 - current result
-        // 16733044 - correct output
+        // x is the number of whole map lens
+        let x = (steps - half_map_len) / map_len;
 
-        // let steps_remaining = steps - (pattern_start as u64 - 1);
-        // let repetitions = steps_remaining / width_height as u64;
-        // let repetitions_remainder = steps_remaining % width_height as u64;
+        let result = a * x.pow(2) + b * x + c;
 
-        // println!("Repetitions: {}, remainder: {}", repetitions, repetitions_remainder);
-        
-        // // Using "natural numbers" formula i.e. sum k=1..n for k = n(n + 1) / 2
-        // let right_sum = repetitions * a_sum + repetitions * (repetitions + 1) / 2 * b_sum;
-        // let left_sum: u64 = counts.iter().enumerate().map(|(i, c)| {
-        //     if i >= pattern_start {
-        //         return 0;
-        //     }
-
-        //     if (i % 2 + 1) as u64 == mod_steps { // TODO: correct?
-        //         return *c as u64;
-        //     } else {
-        //         return 0;
-        //     }
-
-        // }).sum();
-
-        // // TODO: have not even added remainder
-        // let a_partial_sum: u64 = a_all.iter().enumerate().map(|(i, a_single)| {
-        //     if i as u64 >= repetitions_remainder {
-        //         return 0;
-        //     }
-
-        //     if ((a_start_index + i + 1) % 2) as u64 == mod_steps { // TODO: correct?
-        //         *a_single as u64
-        //     } else {
-        //         0
-        //     }
-        // }).sum();
-
-        // let b_partial_sum: u64 = b_all.iter().enumerate().map(|(i, b_single)| {
-        //     if i as u64 >= repetitions_remainder {
-        //         return 0;
-        //     }
-
-        //     if ((pattern_start + i + 1) % 2) as u64 == mod_steps { // TODO: correct?
-        //         *b_single as u64
-        //     } else {
-        //         0
-        //     }
-        // } ).sum();
-
-        // let remainder_sum = (repetitions + 1) * a_partial_sum + (repetitions + 1) * (repetitions + 1 + 1) / 2 * b_partial_sum;
-
-        // let sum = left_sum + right_sum + remainder_sum;
-
-        // println!("Sum is: {}", sum);
-        // 18320514
-        // 15123714
-        // 16733044 - correct output
-
-        return 0; // TODO: Fix
+        return result;
     }
 
     pub fn calculate_garden_plots_reached(&self, steps: u64, infinite: bool) -> u64 {
